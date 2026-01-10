02ed815a15517f6360c441e930cc5e30069385b1
Ignacio Hernaiz <nacho_celtic@hotmail.com>
2026-01-10 19:32:09 +0100

    Initial Commit

---
+++ SuperHeroesSDP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperHeroesSDP
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 10/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperHeroesSDP/SuperHeroesSDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroesSDPApp.swift
+//  SuperHeroesSDP
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 10/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroesSDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
